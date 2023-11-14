9d04ac00b92e6f4ad9e6ae4c410556e8a28bdf27
Ach2290 <132547490+Ach2290@users.noreply.github.com>
2023-11-14 14:24:18 +0100

    Transicion SpalshScreen

--- IbericosCumbres/app/src/main/java/com/example/ibericoscumbres/SplashScreen.java
+++ IbericosCumbres/app/src/main/java/com/example/ibericoscumbres/SplashScreen.java
@@ -4,29 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        openLogin();
 
-
-
-        TextView appNameSplash = findViewById(R.id.appNameSplash);
-
-        Animation fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        appNameSplash.startAnimation(fadeAnimation);
-
-
-    }
     public void openLogin(){
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
@@ -37,6 +25,38 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         }, 3000);
+    }
+  ImageView cerdito_imageview, cumbres_imageview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        openLogin();
+
+        //AGREGAR ANIMACIONES
+        final Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+       final  Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.izquierda);
+
+
+
+        cerdito_imageview = findViewById(R.id.cerdito);
+        cumbres_imageview = findViewById(R.id.cumbres);
+
+        cerdito_imageview.setAnimation(animacion1);
+        cumbres_imageview.setAnimation(animacion2);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                cerdito_imageview.setAnimation(animacion1);
+                cumbres_imageview.setAnimation(animacion2);
+                /*Intent siguiente = new Intent(SplashScreen.this, Login.class);
+                startActivity(siguiente);
+                finish();*/
+            }
+        }, 4000);
     }
 
 }
